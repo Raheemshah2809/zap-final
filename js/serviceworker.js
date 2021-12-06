cda5c45f6dd36c091182ce07a9afce5ea20be52d
Raheem Shah <Raheemshah2809@gmail.com>
2021-12-06 13:33:33 +0000

    1.92

--- js/serviceworker.js
+++ js/serviceworker.js
@@ -2,8 +2,31 @@ const zapapp = "zap-app-v1"
 const assets = [
   "/",
   "/index.html",
-  "/css/*",
-  "/js/*",
+  "/css/main.css",
+  "/css/bootstrap.min.css",
+  "/css/bootstrap.min.css.map",
+  "/js/bootstrap.min.js",
+  "/js/bootstrap.min.js.map",
+  "/js/firebase-analytics-compat.js",
+  "/js/firebase-analytics-compat.js.map",
+  "/js/firebase-app-compat.js",
+  "/js/firebase-app-compat.js.map",
+  "/js/firebase-auth-compat.js",
+  "/js/firebase-auth-compat.js.map",
+  "/js/firebase-database-compat.js",
+  "/js/firebase-database-compat.js.map",
+  "/js/firebase-firestore-compat.js",
+  "/js/firebase-firestore-compat.js.map",
+  "/js/firebase-storage-compat.js",
+  "/js/firebase-storage-compat.js.map",
+  "/js/geol.js",
+  "/js/jquery-3.6.0.min.map",
+  "/js/jquery.min.js",
+  "/js/main.js",
+  "/js/popper.min.js",
+  "/js/popper.min.js.map",
+  "/js/serviceWorker.js",
+  "/manifest.json"
 ]
 
 self.addEventListener("install", installEvent => {
